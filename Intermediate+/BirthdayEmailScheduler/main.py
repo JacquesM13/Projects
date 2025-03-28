e78969d0d1f13c2f8db708bcc21e34cd641dbe5c
JacquesM13 <164623163+JacquesM13@users.noreply.github.com>
2025-03-28 11:09:01 +0000

    Add files via upload

---
+++ Intermediate+/BirthdayEmailScheduler/main.py
@@ -0,0 +1,29 @@
+import pandas
+import datetime as dt
+import random
+import smtplib
+
+MY_EMAIL = "jacquesmassi@gmail.com"
+PASSWORD = "divp mlyg ynfv iupp"
+
+now = dt.datetime.now()
+today_tuple = (now.month, now.day)
+
+data = pandas.read_csv("birthdays.csv")
+
+birthday = data.loc[(data.month == now.month) & (data.day == now.day)]
+
+if birthday.empty:
+    print("Empty")
+else:
+    letter_nr = random.randint(1, 3)
+    with open(f"letter_templates/letter_{letter_nr}.txt") as letter:
+        data = letter.read()
+        data = data.replace("[NAME]", birthday.name.item())
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user= MY_EMAIL, password= PASSWORD)
+        connection.sendmail(from_addr= MY_EMAIL,
+                            to_addrs= birthday.email.item(),
+                            msg= f"Subject: Birthday\n\n{data}")
