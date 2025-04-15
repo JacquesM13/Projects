653b6b319a07ec5f646357dd3f597d39f1ecf85d
JacquesM13 <164623163+JacquesM13@users.noreply.github.com>
2025-04-15 17:21:42 +0100

    Update notification_manager.py
    Added email functionality

--- Capstone/Flight Deal Finder/notification_manager.py
+++ Capstone/Flight Deal Finder/notification_manager.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import smtplib
 
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
@@ -7,6 +8,8 @@ class NotificationManager:
         self.BOT_TOKEN = os.environ.get("BOT_TOKEN")
         self.CHAT_ID = os.environ.get("CHAT_ID")
         self.API_ENDPOINT = f"https://api.telegram.org/bot{self.BOT_TOKEN}/sendMessage"
+        self.PASSWORD = os.environ.get("PASSWORD")
+        self.MY_EMAIL = os.environ.get("MY_EMAIL")
 
 
     def telegram_messenger(self, message):
@@ -17,3 +20,11 @@ class NotificationManager:
         response = requests.get(url= self.API_ENDPOINT, json= parameters)
         response.raise_for_status()
         return response.json()
+
+    def email_sender(self, email_address, message):
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user= self.MY_EMAIL, password= self.PASSWORD)
+            connection.sendmail(from_addr= self.MY_EMAIL,
+                                to_addrs= email_address,
+                                msg= f"Subject: Flight Offer!\n\n{message}")
