95788b5a2c96350da9dd6a05dc919203316686e9
JacquesM13 <164623163+JacquesM13@users.noreply.github.com>
2025-04-15 17:20:30 +0100

    Update main.py
    Added email functionality

--- Capstone/Flight Deal Finder/main.py
+++ Capstone/Flight Deal Finder/main.py
@@ -6,21 +6,27 @@ from notification_manager import NotificationManager
 
 sheetData = DataManager()
 flightSearcher = FlightSearch()
-telegramBot = NotificationManager()
+notificationManager = NotificationManager()
 
 sheet_data = sheetData.get_data()
+sheet_users = sheetData.get_customer_emails()
 
 for city, values in sheet_data.items():
 
     try:
         iata = flightSearcher.fetch_iata(city)
         price, origin, destination, deptDate, returnDate = flightSearcher.get_cheapest_flight(iata)
-        print(f"Flight from London ({origin}) to {city} ({destination}): £{price}, Departs: {deptDate}, Returns: {returnDate}")
+        message = f"Flight from London ({origin}) to {city} ({destination}): {price} GBP, Departs: {deptDate}, Returns: {returnDate}"
+        print(message)
 
     except ValueError:
         print("Try another origin city")
 
     else:
         if float(price) < float(values['lowestPrice']):
-            telegramBot.telegram_messenger(f"Flight from London ({origin}) to {city} ({destination}): £{price}, Departs: {deptDate}, Returns: {returnDate}")
+            notificationManager.telegram_messenger(message)
             sheetData.update_prices(id= values['id'], price= price)
+
+            for user in sheet_users:
+                notificationManager.email_sender(email_address= user, message= message)
+                print(f"Sent to {user}")
